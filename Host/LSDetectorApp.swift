d50a02e547ec150fe24d3dc056e60f38d9176430
Ethan <ethanyoumans@outlook.com>
2025-09-25 17:41:07 -0400

    Initial Project Setup

---
+++ Host/LSDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LSDetectorApp.swift
+//  LSDetector
+//
+//  Created by Youmans, Ethan T. on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LSDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
